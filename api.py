88ee6e79fdfe34d54825db621ce4d032c4c6320e
jezzaayt <gjaytoun@gmail.com>
2024-12-30 19:32:41 +0000

    Adding all into a listbox to display all 40 values of information from OWM.

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 from dotenv import load_dotenv
 import dearpygui.dearpygui as dpg
 import os
-import time
+import datetime
 import requests
 from geopy.geocoders import Nominatim
 load_dotenv('api.env')
@@ -10,6 +10,8 @@ load_dotenv('api.env')
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 def fetch_weather(sender, data):
     city = dpg.get_value("City Name")
+    city = str(city).title()
+    
     unitvalue = dpg.get_value("Measurement")
     print(unitvalue)
     print(city)
@@ -21,46 +23,66 @@ def fetch_weather(sender, data):
     if not geo:
         dpg.set_value("Weather Output", "Please enter a valid city name.")
         return
-   
+    lat = geo[0]
+    lon = geo[1]
+    dpg.set_value("latlon","City:" + city)
+    dpg.set_value("Weather_Conditions","")
     
     BASE_URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units={unitvalue}"
     print(BASE_URL)
     try:
         response = requests.get(BASE_URL)
         weather_data = response.json()
-        
-        print(weather_data["list"][1])
-        print(weather_data["list"][1]["main"])
-
+        output = ""
+        weather_conditions = []
         # Extracting required weather details
-        temp = weather_data["list"][1]["main"]["temp"]
-        print(temp)
-        weather_desc = weather_data["list"][1]["weather"][0]["description"]
-        humidity = weather_data["list"][1]["main"]["humidity"]
-        wind_speed = weather_data["list"][1]["wind"]["speed"]
-        degrees = weather_data["list"][1]["wind"]["deg"]
-        print(temp)
-        # Formatting output
-        if unitvalue =="metric":
-            output = (f"City: {city}\n"
-                    f"Temperature: {temp}°C\n"
-                    f"Condition: {weather_desc}\n"
-                    f"Humidity: {humidity}%\n"
-                    f"Wind Speed: {wind_speed} m/s\n"
-                    f"Degrees: {degrees}°\n")
-        else:
-            
-            output = (f"City: {city}\n"
-                    f"Temperature: {temp}°F\n"
-                    f"Condition: {weather_desc}\n"
-                    f"Humidity: {humidity}%\n"
-                    f"Wind Speed: {wind_speed} m/s\n"
-                    f"Degrees: {degrees}°\n")
-        dpg.set_value("Weather Output", output)
+        for i in range(0,40):
+            temp = weather_data["list"][i]["main"]["temp"]
+            dt = weather_data["list"][i]["dt"]
+            weather_desc = weather_data["list"][i]["weather"][0]["description"]
+            humidity = weather_data["list"][i]["main"]["humidity"]
+            wind_speed = weather_data["list"][i]["wind"]["speed"]
+            degrees = weather_data["list"][i]["wind"]["deg"]
+            date_time = datetime.datetime.utcfromtimestamp(dt) 
+            # Formatting output
+            if unitvalue =="metric":
+                # output += (f"City: {city}\n"
+                #         f"Date {date_time}\n"
+                #         f"Temperature: {temp}°C\n"
+                #         f"Condition: {weather_desc}\n"
+                #         f"Humidity: {humidity}%\n"
+                #         f"Wind Speed: {wind_speed} m/s\n"
+                #         f"Degrees: {degrees}°\n")
+                weather_conditions.append(f"DateTime: {date_time} Temp: {temp}°C Condition: {weather_desc} Humidity: {humidity}% Wind: {wind_speed} m/s Degrees: {degrees}°")
+            elif unitvalue == "imperial":
+                
+                # output += (f"City: {city}\n"
+                #         f"Date {date_time}\n"
+                #         f"Temperature: {temp}°F\n"
+                #         f"Condition: {weather_desc}\n"
+                #         f"Humidity: {humidity}%\n"
+                #         f"Wind Speed: {wind_speed} m/s\n"
+                #         f"Degrees: {degrees}°\n")
+                weather_conditions.append(f"DateTime: {date_time} Temp: {temp}°F Condition: {weather_desc} Humidity: {humidity}% Wind: {wind_speed} mph Degrees: {degrees}°")
+
+        
+            elif unitvalue == "kelvin":
+                weather_conditions.append(f"DateTime: {date_time} Temp: {temp}°K Condition: {weather_desc} Humidity: {humidity}% Wind: {wind_speed} m/s Degrees: {degrees}°")
+
+        
+                # output += (f"City: {city}\n"
+                #         f"Date {date_time}\n"
+                #         f"Temperature: {temp}°K\n"
+                #         f"Condition: {weather_desc}\n"
+                #         f"Humidity: {humidity}%\n"
+                #         f"Wind Speed: {wind_speed} m/s\n"
+                #         f"Degrees: {degrees}°\n")
+        dpg.configure_item("Weather_Conditions", items=weather_conditions)
     except Exception as e:
         dpg.set_value("Weather Output", f"Error: {str(e)}")
         print(e) 
 
+# city to lat long using Nominatim API 
 def city_to_lat_lon(city_name):
     geolocator = Nominatim(user_agent="geoapi")
     location = geolocator.geocode(city_name)
@@ -68,8 +90,6 @@ def city_to_lat_lon(city_name):
         return location.latitude, location.longitude
     else:
         return None, None
-
-# Example usage
 
     latitude, longitude = city_to_lat_lon(city_name)
     if latitude and longitude:
--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import dearpygui.dearpygui as dpg
 from api import *
 
 
-width = 1000
-height = 600
+width = 900
+height = 450
 dpg.create_context()
 dpg.create_viewport(title='Tempest Sky', width=width, height=height)
 dpg.set_viewport_small_icon("icon.ico")  # Small icon for the title bar
 dpg.set_viewport_large_icon("icon.ico")  # Large icon for the taskbar
 dpg.setup_dearpygui()
 
-measurement_list=["metric","imperial"]
+measurement_list=["metric","imperial", "kelvin"]
 
-with dpg.window (width=width, height=height, no_move=True, autosize=True, no_title_bar=True):
+with dpg.window (width=width, height=height, no_move=True,  no_title_bar=True):
     dpg.add_text("Pick Your City")
     dpg.add_input_text(hint="e.g., London", tag="City Name")
     dpg.add_button(label="Search", callback=fetch_weather)
@@ -21,7 +21,8 @@ with dpg.window (width=width, height=height, no_move=True, autosize=True, no_tit
     dpg.add_text("Weather Output:")
     dpg.add_text(tag="latlon", default_value="")
 
-    dpg.add_text(tag = "Weather Output", default_value="")
+    dpg.add_listbox(tag="Weather_Conditions",  default_value="", width=900)
+
 
     
     
